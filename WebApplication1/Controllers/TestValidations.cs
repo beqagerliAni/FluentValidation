9cbfc45bc89990508e7c31eb993c47cd93764b51
beqa gerliani <157922335+beqagerliAni@users.noreply.github.com>
2024-12-21 17:41:05 +0400

    Add project files.

---
+++ WebApplication1/Controllers/TestValidations.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Dto;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestValidations : ControllerBase
+    {
+        [HttpPost]
+        public TestDto Index(TestDto user)
+        {
+            return user;
+        }
+    }
+}
---
+++ WebApplication1/Dto/TestDto.cs
@@ -0,0 +1,44 @@
+﻿using FluentValidation;
+using WebApplication1.Dto;
+
+namespace WebApplication1.Dto
+{
+    public class TestDto
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Passwrod { get; set; }
+        public string RePassword { get; set; }
+    }
+}
+
+public class TestDtoValidator : AbstractValidator<TestDto>
+{
+    public TestDtoValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .WithMessage("Name must not be empty")
+            .NotNull();
+
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .WithMessage("email must not be empty")
+            .EmailAddress()
+            .WithMessage("email must be valid");
+
+        RuleFor(x => x.Passwrod)
+            .NotEmpty()
+            .WithMessage("New password is required.")
+            .MinimumLength(8)
+            .WithMessage("New password must be at least 8 characters long.")
+            .Matches("[A-Z]")
+            .WithMessage("New password must contain at least one uppercase letter.")
+            .Matches("[a-z]")
+            .WithMessage("New password must contain at least one lowercase letter.")
+            .Matches("[0-9]")
+            .WithMessage("New password must contain at least one digit.")
+            .Matches("[^a-zA-Z0-9]")
+            .WithMessage("New password must contain at least one special character.");
+    }
+}
